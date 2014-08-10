096da5b1fca0e3a9dc85766a64da69955295e7f6
knod <knodding@gmail.com>
2014-08-09 22:46:52 -0400

    Removes extraneous slash

--- columns.js
+++ columns.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
 	console.log(process.cwd());
 
 // For the columns, except grid-ins
-var html = jade.renderFile( process.cwd() + '/templates/answer-report.jade'
+var html = jade.renderFile( process.cwd() + 'templates/answer-report.jade'
 	// , 
 	// function(err, html) {
 
